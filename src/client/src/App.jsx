9eb806cf9ac8627873300e50cc43e39a85fd2483
farihaorna23 <farihaorna@gmail.com>
2024-02-20 22:00:33 -0500

    worked on styling and added sign up files

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIN from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/signin" element={<SignIN />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
